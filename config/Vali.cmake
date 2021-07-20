0797c626b5bd34d04fcaa8d7a2a744d8d65f447a
Philip Meulengracht <phailmollen@gmail.com>
2021-07-20 14:28:07 +0200

    Updates to build system, fix the llvm build that everything relies on for vali

--- config/Vali.cmake
+++ config/Vali.cmake
@@ -15,15 +15,28 @@
 
 # Sanitize expected environmental variables
 if(NOT DEFINED ENV{CROSS})
-message(FATAL_ERROR "Please set the CROSS environmental variable to the path of the Vali Crosscompiler.")
+    message(FATAL_ERROR "Please set the CROSS environmental variable to the path of the Vali Crosscompiler.")
 endif()
 
 if(NOT DEFINED ENV{VALI_ARCH})
-message(FATAL_ERROR "Please set the VALI_ARCH environmental variable to the expected platform architecture.")
+    message(WARNING "VALI_ARCH environmental variable was not set, defauling to amd64.")
+    set(ENV{VALI_ARCH} amd64)
 endif()
 
 if(NOT DEFINED ENV{VALI_SDK_PATH})
     message(FATAL_ERROR "Please set the VALI_SDK_PATH environmental variable to the path of the Vali SDK.")
+endif()
+
+# Setup expected environment variables
+set(ENV{VALI_INCLUDES}  "-I$ENV{VALI_SDK_PATH}/include/clang-9.0.0 -I$ENV{VALI_SDK_PATH}/include")
+set(ENV{VALI_LIBRARIES} "-LIBPATH:$ENV{VALI_SDK_PATH}/lib")
+set(ENV{VALI_SDK_CLIBS}   "static_libcrt.lib static_librt.lib c.lib m.lib")
+set(ENV{VALI_SDK_CXXLIBS} "$ENV{VALI_SDK_CLIBS} static_c++.lib static_c++abi.lib unwind.lib")
+
+if("$ENV{VALI_ARCH}" STREQUAL "i386")
+    set(ENV{VALI_LFLAGS} "/machine:X86 /nodefaultlib /subsystem:native /lldmap $ENV{VALI_LIBRARIES}")
+else()
+    set(ENV{VALI_LFLAGS} "/machine:X64 /nodefaultlib /subsystem:native /lldmap $ENV{VALI_LIBRARIES}")
 endif()
 
 # Setup environment stuff for cmake configuration
@@ -36,8 +49,8 @@ set(CMAKE_AR "$ENV{CROSS}/bin/llvm-ar" CACHE FILEPATH "")
 set(CMAKE_RANLIB "$ENV{CROSS}/bin/llvm-ranlib" CACHE FILEPATH "")
 set(VERBOSE 1)
 
-# Setup expected environment variables
-set(ENV{VALI_LIBRARIES} "-LIBPATH:$ENV{VALI_SDK_PATH}/lib")
+set(CMAKE_C_COMPILER_WORKS 1)
+set(CMAKE_CXX_COMPILER_WORKS 1)
 
 # Setup shared compile flags to make compilation succeed
 set(VALI_COMPILE_FLAGS -U_WIN32 -fms-extensions -nostdlib -nostdinc -static -DMOLLENOS -DZLIB_DLL)
