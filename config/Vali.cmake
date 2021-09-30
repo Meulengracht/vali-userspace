0f1ffafd2ee5cdfaac4baf80f4bc3b7710a279cb
Philip Meulengracht <the_meulengracht@hotmail.com>
2021-09-30 09:01:14 +0200

    vali: llvm now builds, everything should configure correctly

--- config/Vali.cmake
+++ config/Vali.cmake
@@ -27,10 +27,6 @@ if(NOT DEFINED ENV{VALI_SDK_PATH})
     message(FATAL_ERROR "Please set the VALI_SDK_PATH environmental variable to the path of the Vali SDK.")
 endif()
 
-# Setup expected environment variables
-set(ENV{VALI_LIBRARIES} "-libpath:$ENV{VALI_SDK_PATH}/lib")
-set(ENV{VALI_LFLAGS} "-lldmap -lldvpe $ENV{VALI_LIBRARIES}")
-
 # Setup environment stuff for cmake configuration
 set(CMAKE_SYSTEM_NAME valicc)
 set(CMAKE_SYSTEM_VERSION 1)
@@ -41,13 +37,6 @@ set(CMAKE_AR "$ENV{CROSS}/bin/llvm-ar" CACHE FILEPATH "")
 set(CMAKE_RANLIB "$ENV{CROSS}/bin/llvm-ranlib" CACHE FILEPATH "")
 set(VERBOSE 1)
 
+# Unfortunately we have to set those up since the c++ runtime is not built yet at this point
 set(CMAKE_C_COMPILER_WORKS 1)
 set(CMAKE_CXX_COMPILER_WORKS 1)
-
-# We need to preserve any flags that were passed in by the user. However, we
-# can't append to CMAKE_C_FLAGS and friends directly, because toolchain files
-# will be re-invoked on each reconfigure and therefore need to be idempotent.
-# The assignments to the _INIT cache variables don't use FORCE, so they'll
-# only be populated on the initial configure, and their values won't change
-# afterward.
-#string(APPEND CMAKE_CXX_FLAGS_INIT " -fstatic-runtime-cxx -static-libstdc++")
