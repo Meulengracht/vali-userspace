a3aab780b379bf6ccfc5c4289d3d43c89a6a4b57
Philip Meulengracht <phailmollen@gmail.com>
2021-11-14 21:11:35 +0100

    add mako python package

--- depends.sh
+++ depends.sh
@@ -3,6 +3,6 @@ SCRIPT=`realpath $0`
 SCRIPTPATH=`dirname $SCRIPT`
 
 apt-get update
-apt-get -y -qq install bison flex python3 python3-pip
+apt-get -y -qq install bison flex python3 python3-pip python3-mako
 pip3 install prettytable Mako pyaml dateutils --upgrade
 pip3 install --upgrade git+https://github.com/meulengracht/glad.git#egg=glad
